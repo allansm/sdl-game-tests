9ccbf7d3313f055c1e87f696dbca1a9a364a18af
allansm <37338692+allansm@users.noreply.github.com>
2023-01-21 12:10:29 -0300

    Add first player

--- pong/pong.cpp
+++ pong/pong.cpp
@@ -10,23 +10,17 @@ void clear(SDL_Renderer* render){
 }
 
 bool collide(SDL_Rect& r1, SDL_Rect& r2){
-	if(r1.x + r1.w/2 <= r2.x - r2.w/2){
-		return false;
+	if(r1.x < r2.x + r2.w){
+		if(r1.x + r1.w > r2.x){
+			if(r1.y < r2.y + r2.h){
+				if(r1.y + r1.h > r2.y){
+					return true;
+				}
+			}
+		}
 	}
 	
-	if(r1.x - r1.w/2 >= r2.x + r2.w/2){
-		return false;
-	}
-	
-	if(r1.y + r1.h/2 <= r2.y - r2.h/2){
-		return false;
-	}
-
-	if(r1.y - r1.h/2 >= r2.y + r2.h/2){
-		return false;
-	}
-	
-	return true;
+	return false;
 }
 
 struct Ball{
@@ -44,6 +38,84 @@ struct Ball{
 		SDL_SetRenderDrawColor(render, 255, 255, 255, 255);
 		SDL_RenderFillRect(render, &rect);
 	}
+	
+	bool hit(SDL_Rect& rect){
+		return collide(this->rect, rect);
+	}
+};
+
+struct Block{
+	SDL_Rect rect;
+
+	Block(int x, int y, int w, int h){
+		rect = {x, y, w, h};
+	}
+	
+	void move(int x ,int y){
+		rect.x += x;
+		rect.y += y;
+	}
+	
+	void draw(SDL_Renderer* render){
+		SDL_SetRenderDrawColor(render, 255, 255, 255, 255);
+		SDL_RenderFillRect(render, &rect);
+	}
+	
+	bool hit(SDL_Rect& rect){
+		return collide(this->rect, rect);
+	}
+};
+
+struct Player{
+	Block top = {25, 0, 8, 8*3};
+	Block mid = {25, 8*3, 8, 8*3};
+	Block bot = {25, 8*3*2, 8, 8*3};
+	
+	int speed = 20;
+	int atk_speed = 10;
+	
+	void draw(SDL_Renderer* render){
+		top.draw(render);
+		mid.draw(render);
+		bot.draw(render);
+	}
+
+	void hit(Ball& ball){
+		if(top.hit(ball.rect)){
+			ball.speed_x *= -atk_speed;
+			ball.speed_y = 1;
+		}
+
+		if(mid.hit(ball.rect)){
+			ball.speed_x *= -atk_speed;
+			ball.speed_y = 0;
+		}
+		
+		if(bot.hit(ball.rect)){
+			ball.speed_x *= -atk_speed;
+			ball.speed_y = -1;
+		}
+	}
+
+	void move(int x, int y){
+		top.move(x, y);
+		mid.move(x, y);
+		bot.move(x, y);
+	}
+	
+	void move(SDL_Event& event){
+		switch(event.type){
+			case SDL_KEYDOWN:
+				switch(event.key.keysym.sym){
+					case SDLK_w:
+						move(0, speed*-1);
+						break;
+					case SDLK_s:
+						move(0, speed);
+						break;
+				}
+		}
+	}
 };
 
 
@@ -59,37 +131,44 @@ int main(){
 	SDL_Event event;
 
 	Ball ball;
+	Player p1;
 	
-	SDL_Rect left = {0, 0, 1, 600};
-	SDL_Rect right = {799, 0, 1, 600};
-	
-	int sx = 1;
+	SDL_Rect left = {0, 0, 5, 600};
+	SDL_Rect right = {799, 0, 5, 600};
+	SDL_Rect top = {0, 0, 800, 5};
+	SDL_Rect bottom = {0, 599, 800, 5};
 
 	while(!quit){
 		clear(render);
 
 		ball.draw(render);
+		p1.draw(render);
 		
 		SDL_RenderPresent(render);
 
-		if(collide(left, ball.rect)){
-			sx+=2;
-			if(sx > 10) sx = 1;
+		if(ball.hit(left)){
+			ball.speed_x = 1;
+		}
+
+		if(ball.hit(right)){
+			ball.speed_x = -1;
+		}
+
+		if(ball.hit(top)){
+			ball.speed_y = 1;
+		}
+		
+		if(ball.hit(bottom)){
+			ball.speed_y = -1;
+		}
 	
-			ball.speed_x = sx;
-		}
-
-		if(collide(right, ball.rect)){
-			sx+=2;
-			
-			if(sx > 10) sx = 1;
-
-			ball.speed_x = sx*-1;
-		}
+		p1.hit(ball);
 
 		ball.move();
 		
 		while(SDL_PollEvent(&event)){
+			p1.move(event);		
+	
 			if(event.type == SDL_QUIT){
 				quit = true;
 			}
